91c801a5e54412784424f85aad8cca8de498ca18
wellcome2AD <iuuyyttttt@mail.ru>
2022-12-09 12:03:08 +0300

    попытки сделать код более читаемым

--- db.js
+++ db.js
@@ -4,4 +4,85 @@ var conString = "postgres://postgres:1234@localhost:5432/my_app";
 var client = new pg.Client(conString);
 client.connect();
 
+client.listOfProblemsQuery = function(req, res) {
+    var cookie = req.cookies;
+    var data = {};
+    data.role = undefined;
+    if(cookie.login != undefined && cookie.password != undefined) 
+    {
+        client.query("SELECT id, role FROM public.user WHERE login LIKE $1 AND password LIKE $2;", [cookie.login, cookie.password], function(err, rows){ 
+            if (err){
+                console.log(err); 
+                return;
+            }
+            if(rows.rowCount != 0) {
+                var role = rows.rows[0].role;
+                var id = rows.rows[0].id;
+                data.role = role;
+                if(role == "admin")
+                {
+                    client.query("SELECT list_of_problems.id, fio, login, phone, problem_desc, is_done, comment " +
+                                 "FROM list_of_problems " +
+                                 "LEFT JOIN public.user ON (user_id = public.user.id) " +
+                                 "WHERE is_done = false " +
+                                 "ORDER BY list_of_problems.id;", 
+                    function(err, rows){ 
+                        if (err){
+                            console.log(err); 
+                            return;
+                        }
+                        data.rows = rows;
+                        res.end(JSON.stringify(data));
+                    });
+                }
+                else if(role == "employee")
+                {
+                    client.query("SELECT problem_id, problem_desc, is_done, comment " +
+                                "FROM task_distrib " +
+                                "RIGHT JOIN list_of_problems ON (problem_id = list_of_problems.id) " +
+                                "WHERE (task_distrib.employee_id = $1);", [id],
+                        function(err, rows){ 
+                            if (err){
+                                console.log(err); 
+                                return;
+                            }
+                            data.rows = rows;
+                            res.end(JSON.stringify(data));
+                        });
+                }
+                else
+                {                    
+                    client.query("SELECT list_of_problems.id, fio, login, phone, problem_desc, is_done, comment " + 
+                                 "FROM list_of_problems " +
+                                 "LEFT JOIN public.user ON (user_id = public.user.id) " +
+                                 "ORDER BY list_of_problems.id;", 
+                        function(err, rows){
+                            if (err){
+                                console.log(err); 
+                                return;
+                            }
+                            data.rows = rows;
+                            res.end(JSON.stringify(data));
+                    });
+                }
+            }
+        });
+    }
+    else
+    {
+        client.query("SELECT list_of_problems.id, fio, login, phone, problem_desc, is_done, comment " + 
+                     "FROM list_of_problems " +
+                     "LEFT JOIN public.user ON (user_id = public.user.id) " +
+                     "ORDER BY list_of_problems.id;", 
+        function(err, rows){
+            if (err){
+                console.log(err); 
+                return;
+            }
+            data.rows = rows;
+            res.end(JSON.stringify(data));
+        });
+    }
+}
+
 module.exports = client;
--- my_server.js
+++ my_server.js
@@ -4,10 +4,10 @@ const url = require('url');
 const http = require('http');
 const cookieParser = require('cookie-parser');
 const WebSocket = require('ws');
+var bodyParser = require('body-parser');
 
 var client = require("./db");
-
-var bodyParser = require('body-parser');
+const router = require('./my_router');
 
 const app = express();
 
@@ -16,6 +16,8 @@ app.use(bodyParser.json());
 
 app.use(express.static('public', {extensions:['html']})); // статическое содержимое - в public, которая находится в корне проекта
 app.use(cookieParser());
+
+app.use('/', router);
 
 const EE = require("events").EventEmitter;
 
@@ -87,7 +89,6 @@ wss.on('connection', function connection(ws, req) {
 
     const on_new_problem = (message) => {
         var user_id = null;
-        console.log(message);
         if(message.data.cookie != undefined)
         {
             var cookie = message.data.cookie;
@@ -132,7 +133,6 @@ wss.on('connection', function connection(ws, req) {
 
     const task_distrib_update = (message) => {
         var message_data = JSON.parse(message.data.data);
-        console.log(message_data);
         for(var i = 0; i < message_data.employee_id.length; ++i)
         {
             if(message_data.employee_id[i] != -1)
@@ -164,7 +164,6 @@ wss.on('connection', function connection(ws, req) {
             var list_of_users = webSockets[key_to_send]
             for(var i = 0; i < list_of_users.length; ++i)
             {
-                console.log(list_of_users[0].cookie);
                 var optionList = list_of_users[0].cookie.split(';'); // ["login=admin", "password=qwertyu"]
                 var loginOption = optionList[0].split('=');
                 var login = loginOption[1];
@@ -226,7 +225,6 @@ wss.on('connection', function connection(ws, req) {
 
     const list_of_problems_update = (message) => {
         var message_data = JSON.parse(message.data.data);
-        console.log(message_data);
         for(var i = 0; i < message_data.problem_id.length; ++i)
         {
             client.query("UPDATE list_of_problems SET is_done=$1, comment=$2 WHERE id=$3;", [message_data.is_done[i], message_data.comment[i], message_data.problem_id[i]],
@@ -307,137 +305,6 @@ wss.on('connection', function connection(ws, req) {
 
         }
     })
-});
-
-app.get("/", function(req, res){
-    res.redirect("/form.html");
-})
-app.get("/list_of_problems.html", function(req,res){
-    var cookie = req.cookies;
-    var data = {};
-    data.role = undefined;
-    if(cookie.login != undefined && cookie.password != undefined) 
-    {
-        client.query("SELECT id, role FROM public.user WHERE login LIKE $1 AND password LIKE $2;", [cookie.login, cookie.password], function(err, rows){ 
-            if (err){
-                console.log(err); 
-                return;
-            }
-            if(rows.rowCount != 0) {
-                var role = rows.rows[0].role;
-                var id = rows.rows[0].id;
-                data.role = role;
-                if(role == "admin")
-                {
-                    client.query("SELECT * FROM list_of_problems WHERE is_done = false ORDER BY id;", function(err, rows){ 
-                        if (err){
-                            console.log(err); 
-                            return;
-                        }
-                        data.rows = rows;
-                        res.end(JSON.stringify(data));
-                    });
-                }
-                else if(role == "employee")
-                {
-                    client.query("SELECT problem_id, problem_desc, is_done, comment " +
-                                "FROM task_distrib " +
-                                "JOIN list_of_problems ON (problem_id = list_of_problems.id) " +
-                                "WHERE (task_distrib.employee_id = $1);", [id],
-                        function(err, rows){ 
-                            if (err){
-                                console.log(err); 
-                                return;
-                            }
-                            data.rows = rows;
-                            res.end(JSON.stringify(data));
-                        });
-                }
-                else
-                {                    
-                    client.query("SELECT list_of_problems.id, fio, login, phone, problem_desc, is_done, comment " + 
-                             "FROM list_of_problems " +
-                             "LEFT JOIN public.user ON (user_id = public.user.id) " +
-                             "ORDER BY list_of_problems.id;", 
-                        function(err, rows){
-                            if (err){
-                                console.log(err); 
-                                return;
-                            }
-                            data.rows = rows;
-                            res.end(JSON.stringify(data));
-                    });
-                }
-            }
-        });
-    }    
-    client.query("SELECT list_of_problems.id, fio, login, phone, problem_desc, is_done, comment " + 
-                            "FROM list_of_problems " +
-                            "LEFT JOIN public.user ON (user_id = public.user.id) " +
-                            "ORDER BY list_of_problems.id;", 
-        function(err, rows){
-            if (err){
-                console.log(err); 
-                return;
-            }
-            data.rows = rows;
-            res.end(JSON.stringify(data));
-    });
-});
-
-app.get("/task_distribution.html", function(req,res){
-    var cookie = req.cookies;
-    var data = {};
-    data.haveAccess = false;
-    data.info = [];
-    data.options_for_select = [];
-    data.table_data = [];
-    if(cookie.login != undefined && cookie.password != undefined) 
-    {
-        client.query("SELECT role FROM public.user WHERE login LIKE $1 AND password LIKE $2;", [cookie.login, cookie.password], function(err, rows){ 
-            if (err){
-                console.log(err); 
-                return;
-            }
-            if(rows.rowCount == 0) {
-                data.haveAccess = true;
-                data.info = "Список проблем пуст. Можно отдохнуть"
-                res.end(JSON.stringify(data));
-            }
-            var role = rows.rows[0].role;            
-            if(role == "!admin")
-            {
-                data.haveAccess = true;                
-                data.info = 'Нет доступа к странице';
-                res.end(JSON.stringify(data));                  
-            }
-            data.haveAccess = 1;
-            client.query("SELECT id, login FROM public.user WHERE role='employee';", function(err, rows){ 
-                if (err){
-                    console.log(err); 
-                    return;
-                }
-                data.options_for_select = JSON.stringify(rows);
-
-                client.query("SELECT list_of_problems.id, problem_desc, employee_id " +
-                             "FROM list_of_problems LEFT JOIN task_distrib ON (problem_id = list_of_problems.id) " +
-                             "WHERE is_done = false ORDER BY list_of_problems.id;", 
-                    function(err, rows){ 
-                        if (err){
-                            console.log(err); 
-                            return;
-                        }
-                        data.table_data = JSON.stringify(rows);
-                        res.end(JSON.stringify(data));
-                });
-            });
-        });
-    }
-    else {
-        data.haveAccess = false;
-        data.info = 'Нет доступа к странице';
-        res.end(JSON.stringify(data));
-    }
 });
 
 app.listen(3000); //связали с портом и запустили сервер
